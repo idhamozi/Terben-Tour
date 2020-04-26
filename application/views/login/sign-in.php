7b3c74c0577e5361215566ba579493c20f018451
andresaputra <andresaputra@gmail.com>
2020-04-26 13:42:23 +0700

    Responsive page
    Sign-in & Sign-up

--- application/views/login/sign-in.php
+++ application/views/login/sign-in.php
@@ -2,141 +2,135 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
-    <link rel="stylesheet" href="<?= base_url('assets\frontend\libraries\bootstrap\css\bootstrap.css')  ?>">
-    <link
-        href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
-        rel="stylesheet">
-    <link href="<?= base_url('assets\frontend\libraries\font-awesome-5\css\fontawesome-all.min.css')  ?>" rel="stylesheet" media="all">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets\frontend\styles\main.css')  ?>">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sign In</title>
+	<link rel="stylesheet" href="<?= base_url('assets\frontend\libraries\bootstrap\css\bootstrap.css')  ?>">
+	<link href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<link href="<?= base_url('assets\frontend\libraries\font-awesome-5\css\fontawesome-all.min.css')  ?>" rel="stylesheet" media="all">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets\frontend\styles\main.css')  ?>">
 </head>
 
 <body>
-  <?php
-  // Notifikasi error
-  echo validation_errors('<p class="alert alert-warning">','</p>');
+	<?php
+	// Notifikasi error
+	echo validation_errors('<p class="alert alert-warning">', '</p>');
 
-  // Form open
-  echo form_open(base_url('login/LoginCommon'));
-  ?>
-    <section class="">
-        <img src="<?= base_url('assets/frontend/images/bg-sign_in.png')  ?>" class="img-fluid section-bg-login" alt="">
-    </section>
-    <section class="section-sign_in">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="card card-form">
-                        <div class="logo-sign_in">
-                            <img src="<?= base_url('assets/frontend/images/logo-sing_in.png')  ?>" class="rounded" alt="">
-                        </div>
-                        <div class="login-form">
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label>Username</label>
-                                    <input type="username" name="username" class="form-control" placeholder="Username">
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input type="password" name="password" class="form-control" placeholder="Password">
-                                </div>
+	// Form open
+	echo form_open(base_url('login/LoginCommon'));
+	?>
+	<section class="section-header-sign_in">
+		<section class="section-sign_in">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="card card-form">
+							<div class="logo-sign_in">
+								<img src="<?= base_url('assets/frontend/images/logo-sing_in.png')  ?>" class="rounded" alt="">
+							</div>
+							<div class="login-form">
+								<form action="" method="post">
+									<div class="form-group">
+										<label>Username</label>
+										<input type="username" name="username" class="form-control" placeholder="Username">
+									</div>
+									<div class="form-group">
+										<label>Password</label>
+										<input type="password" name="password" class="form-control" placeholder="Password">
+									</div>
 
-                                <div class="form-group form-check">
-                                    <input type="checkbox" class="form-check-input">
-                                    <label class="form-check-label mb-2">Remember Me</label>
-                                    <label class=" mb-2">
-                                        <a href="#" class="check-forget" data-toggle="modal"
-                                                data-target="#LupaPassword">Lupa Password ?</a>
-                                    </label>
-                                </div>
-                                <button class="btn sign_in" type="submit">Sign In</button>
-                                <div class="social-login-content mb-2">
-                                    <div class="social-button">
-                                        <a href="
+									<div class="form-group form-check">
+										<input type="checkbox" class="form-check-input">
+										<label class="form-check-label mb-2">Remember Me</label>
+										<label class=" mb-2">
+											<a href="#" class="check-forget" data-toggle="modal" data-target="#LupaPassword">Lupa Password ?</a>
+										</label>
+									</div>
+									<button class="btn sign_in" type="submit">Sign In</button>
+									<div class="social-login-content mb-2">
+										<div class="social-button">
+											<a href="
                                         <?php
-                                        if (!isset($login_button)) {
-                                          $user_data = $this->session->userdata('user_data');
-                                        } else {
-                                          echo $login_button;
-                                        } ?>
-                                        " class="fb btn"><i class="fa fa-google fa-fw"></i> Login with Google
-                                          </a>
-                                    </div>
-                                </div>
-                            </form>
-                            <div class="container-fluid">
-                                <div class="row border-top justify-content-center align-items-center pt-3">
-                                    <div class="col-auto text-gray-500-font-weight-light">
-                                        <p>
-                                            Don't you have account?
-                                            <a href="<?= base_url('login/register')  ?>">Sign Up Here</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-    </section>
-    <?php
-    // Form close
-    echo form_close();
-    ?>
+										if (!isset($login_button)) {
+											$user_data = $this->session->userdata('user_data');
+										} else {
+											echo $login_button;
+										} ?>
+                                        " class="google btn"><i class="fa fa-google fa-fw"></i> Login with Google
+											</a>
+										</div>
+									</div>
+								</form>
+								<div class="container-fluid">
+									<div class="row border-top justify-content-center align-items-center pt-3">
+										<div class="col-auto text-gray-500-font-weight-light">
+											<p>
+												Don't you have account?
+												<a href="<?= base_url('login/register')  ?>">Sign Up Here</a>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</section>
+	</section>
+	<?php
+	// Form close
+	echo form_close();
+	?>
 
-<!-- Modal - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
-    <div class="modal fade" id="LupaPassword" tabindex="-1" role="dialog"
-        aria-labelledby="LupaPasswordLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="LupaPasswordLabel">Lupa Password
-                    </h5>
-                </div>
-                <div class="modal-body">
-                    <Form>
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label>Email address</label>
-                                <input type="email" class="form-control" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <label>Password Baru</label>
-                                <input type="password" class="form-control" placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label>Konfirmasi Password</label>
-                                <input type="password" class="form-control" placeholder="Password">
-                            </div>
-                    </Form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary"
-                        data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary"> Save </button>
-                </div>
-            </div>
-        </div>
-    </div>
-<!-- end modal - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
+	<!-- Modal - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
+	<div class="modal fade" id="LupaPassword" tabindex="-1" role="dialog" aria-labelledby="LupaPasswordLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="LupaPasswordLabel">Lupa Password
+					</h5>
+				</div>
+				<div class="modal-body">
+					<Form>
+						<form action="" method="post">
+							<div class="form-group">
+								<label>Email address</label>
+								<input type="email" class="form-control" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<label>Password Baru</label>
+								<input type="password" class="form-control" placeholder="Password">
+							</div>
+							<div class="form-group">
+								<label>Konfirmasi Password</label>
+								<input type="password" class="form-control" placeholder="Password">
+							</div>
+						</Form>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+					<button type="button" class="btn btn-primary"> Save </button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end modal - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
 
-    <!-- SWEETALERT -->
-    <?php if($this->session->flashdata('sukses')) { ?>
-    <script>
-      swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>","success")
-    </script>
-    <?php } ?>
+	<!-- SWEETALERT -->
+	<?php if ($this->session->flashdata('sukses')) { ?>
+		<script>
+			swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>", "success")
+		</script>
+	<?php } ?>
 
-    <?php if($this->session->flashdata('warning')) { ?>
-    <script>
-      swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>","warning")
-    </script>
-    <?php } ?>
+	<?php if ($this->session->flashdata('warning')) { ?>
+		<script>
+			swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>", "warning")
+		</script>
+	<?php } ?>
 </body>
 
 <script src="<?= base_url('assets\frontend\libraries\jquery\jquery-3.4.1.min.js')  ?>"></script>
@@ -145,13 +139,13 @@
 <!-- iCheck -->
 <script src="<?php echo base_url('assets\frontend\plugin\iCheck\icheck.min.js') ?>"></script>
 <script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass   : 'iradio_square-blue',
-      increaseArea : '20%' // optional
-    })
-  })
+	$(function() {
+		$('input').iCheck({
+			checkboxClass: 'icheckbox_square-blue',
+			radioClass: 'iradio_square-blue',
+			increaseArea: '20%' // optional
+		})
+	})
 </script>
 
 </html>
--- application/views/register/sign-up.php
+++ application/views/register/sign-up.php
@@ -2,122 +2,119 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('assets\frontend\libraries\bootstrap\css\bootstrap.css')  ?>">
-    <link
-        href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
-        rel="stylesheet">
-    <link href="<?= base_url('assets\frontend\libraries\font-awesome-5\css\fontawesome-all.min.css')  ?>" rel="stylesheet" media="all">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets\frontend\styles\main.css')  ?>">
-    <title>Sign Up</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?= base_url('assets\frontend\libraries\bootstrap\css\bootstrap.css')  ?>">
+	<link href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<link href="<?= base_url('assets\frontend\libraries\font-awesome-5\css\fontawesome-all.min.css')  ?>" rel="stylesheet" media="all">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets\frontend\styles\main.css')  ?>">
+	<title>Sign Up</title>
 </head>
 
 <?php
 
 // Notifikasi error
-echo validation_errors('<p class="alert alert-warning">','</p>');
+echo validation_errors('<p class="alert alert-warning">', '</p>');
 
 // Form open
 echo form_open(base_url('login/register'));
 
- ?>
+?>
 
 <body>
-    <section class="">
-        <img src="<?= base_url('assets/frontend/images/bg-sign_up.png')  ?>" class="img-fluid section-bg-sign_up" alt="">
-    </section>
-    <Section class="section-sign_up">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="content-text">
-                        <div class="text-header">
-                            <h1 class="text-welcome">
-                                Welcome to
-                            </h1>
-                            <h1 class="text-travel">
-                                Terben.Tour Travel
-                            </h1>
-                        </div>
-                        <div class="text-banner">
-                            <h2>
-                                Travel Aman <br>
-                                Harga Nyaman
-                            </h2>
-                            <p>Pusing dengan Kerjaan anda? Liburan Yuk!</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card card-sign_up">
-                        <div class="logo-sign_up mx-4 my-3">
-                            <img src="<?= base_url('assets/frontend/images/logo-sign_up.png')  ?>" class="rounded" alt="">
-                        </div>
-                        <div class="sign_up-form mx-4">
-                            <form action="" method="post">
-                                <div class="form-group mb-3">
-                                    <label>Name</label>
-                                    <div class="form-row">
-                                        <div class="col">
-                                            <input type="text" name="first_name" value="<?php echo set_value('first_name'); ?>" class="form-control" placeholder="First name">
-                                        </div>
-                                        <div class="col">
-                                            <input type="text" name="last_name" value="<?php echo set_value('last_name'); ?>" class="form-control" placeholder="Last name">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group mb-3">
-                                    <label>Username</label>
-                                    <input type="username" name="username" value="<?php echo set_value('username'); ?>" class="form-control" placeholder="Username">
-                                </div>
-                                <div class="form-group mb-3">
-                                    <label>Email address</label>
-                                    <input type="email" name="email" value="<?php echo set_value('email'); ?>" class="form-control" placeholder="Email">
-                                </div>
-                                <div class="form-group mb-3">
-                                    <label>Password</label>
-                                    <input type="password" name="password" value="<?php echo set_value('password'); ?>" class="form-control" placeholder="Password">
-                                </div>
-                                <!-- <div class="form-group mb-3">
+	<section class="section-header-sign_up">
+		<Section class="section-sign_up">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="content-text">
+							<div class="text-header">
+								<h1 class="text-welcome">
+									Welcome to
+								</h1>
+								<h1 class="text-travel">
+									Terben.Tour Travel
+								</h1>
+							</div>
+							<div class="text-banner">
+								<h2>
+									Travel Aman <br>
+									Harga Nyaman
+								</h2>
+								<p>Pusing dengan Kerjaan anda? Liburan Yuk!</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="card card-sign_up">
+							<div class="logo-sign_up mx-4 my-3">
+								<img src="<?= base_url('assets/frontend/images/logo-sign_up.png')  ?>" class="rounded" alt="">
+							</div>
+							<div class="sign_up-form mx-4">
+								<form action="" method="post">
+									<div class="form-group mb-3">
+										<label>Name</label>
+										<div class="form-row">
+											<div class="col">
+												<input type="text" name="first_name" value="<?php echo set_value('first_name'); ?>" class="form-control" placeholder="First name">
+											</div>
+											<div class="col">
+												<input type="text" name="last_name" value="<?php echo set_value('last_name'); ?>" class="form-control" placeholder="Last name">
+											</div>
+										</div>
+									</div>
+									<div class="form-group mb-3">
+										<label>Username</label>
+										<input type="username" name="username" value="<?php echo set_value('username'); ?>" class="form-control" placeholder="Username">
+									</div>
+									<div class="form-group mb-3">
+										<label>Email address</label>
+										<input type="email" name="email" value="<?php echo set_value('email'); ?>" class="form-control" placeholder="Email">
+									</div>
+									<div class="form-group mb-3">
+										<label>Password</label>
+										<input type="password" name="password" value="<?php echo set_value('password'); ?>" class="form-control" placeholder="Password">
+									</div>
+									<!-- <div class="form-group mb-3">
                                     <label>Foto Profil</label>
                                     <input type="file" class="form-control-file">
                                 </div> -->
-                                <button class="btn sign_up" type="submit">Register</button>
-                            </form>
-                            <div class="container-fluid mt-3">
-                                <div class="row border-top justify-content-center align-items-center pt-3">
-                                    <div class="col-auto text-gray-500-font-weight-light">
-                                        <p>
-                                            Do you have account?
-                                            <a href="<?= base_url('Login')  ?>">Sign In Here</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </Section>
-    <?php
-    // Form close
-    echo form_close();
-    ?>
+									<button class="btn sign_up" type="submit">Register</button>
+								</form>
+								<div class="container-fluid mt-3">
+									<div class="row border-top justify-content-center align-items-center pt-3">
+										<div class="col-auto text-gray-500-font-weight-light">
+											<p>
+												Do you have account?
+												<a href="<?= base_url('Login')  ?>">Sign In Here</a>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		</Section>
+	</section>
+	<?php
+	// Form close
+	echo form_close();
+	?>
 
-    <!-- SWEETALERT -->
-    <?php if($this->session->flashdata('sukses')) { ?>
-    <script>
-      swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>","success")
-    </script>
-    <?php } ?>
+	<!-- SWEETALERT -->
+	<?php if ($this->session->flashdata('sukses')) { ?>
+		<script>
+			swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>", "success")
+		</script>
+	<?php } ?>
 
-    <?php if($this->session->flashdata('warning')) { ?>
-    <script>
-      swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>","warning")
-    </script>
-    <?php } ?>
+	<?php if ($this->session->flashdata('warning')) { ?>
+		<script>
+			swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>", "warning")
+		</script>
+	<?php } ?>
 </body>
 
 <script src="<?= base_url('assets\frontend\libraries\jquery\jquery-3.4.1.min.js')  ?>"></script>
@@ -126,13 +123,13 @@ echo form_open(base_url('login/register'));
 <!-- iCheck -->
 <script src="<?php echo base_url('assets\frontend\plugin\iCheck\icheck.min.js') ?>"></script>
 <script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass   : 'iradio_square-blue',
-      increaseArea : '20%' // optional
-    })
-  })
+	$(function() {
+		$('input').iCheck({
+			checkboxClass: 'icheckbox_square-blue',
+			radioClass: 'iradio_square-blue',
+			increaseArea: '20%' // optional
+		})
+	})
 </script>
 
 </html>
